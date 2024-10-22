542d4b2921d41d647f4c0fc16f58d9fd21a5fd78
Eugene Lee <yvjnlee@gmail.com>
2024-10-21 23:36:35 -0400

    testing pt2

--- backend/server/common/server.ts
+++ backend/server/common/server.ts
@@ -27,22 +27,29 @@ export default class ExpressServer {
     app.use(cookieParser(process.env.SESSION_SECRET));
 
     // Apply CORS before any other middleware
-    const corsOptions = {
-      origin: process.env.NODE_ENV === 'production'
-        ? 'https://estiafrontend.vercel.app'
-        : ['https://estiafrontend.vercel.app', 'http://localhost:8080'],
-      credentials: true,
-    };
-    app.use(cors(corsOptions));
-
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'production') {
+      app.use(cors({
+        origin: 'https://estiafrontend.vercel.app', // Specific frontend URL
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+      }));
+    } else {
+      app.use(cors({ origin: '*' }));  // Open for all in development
       app.use(express.static(`${root}/public`));
     }
 
     // Add a route to serve the api.yml Swagger spec file
     app.get('/api/v1/spec', (_, res) => {
-      const specPath = path.join(__dirname, '../../public/api-explorer/api.yml');
+      // Ensure the correct path is served both locally and in production
+      const specPath = process.env.NODE_ENV === 'production'
+        ? path.join(__dirname, '../../public/api-explorer/api.yml')
+        : path.join(__dirname, '../../public/api-explorer/api.yml');
+
+      // Log the path to verify on production
       console.log('Serving Swagger spec from:', specPath);
+
+      // Apply CORS headers directly to the spec file response
+      res.header('Access-Control-Allow-Origin', 'https://estiafrontend.vercel.app');
       res.sendFile(specPath);
     });
   }
